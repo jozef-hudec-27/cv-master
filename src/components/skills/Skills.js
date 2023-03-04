19fad0628c896dc6b5e0777f1a636affafd6e0b8
Jozef Hudec <jojohudec27@gmail.com>
2023-03-04 09:37:51 +0100

    Give users ability to add own skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLaptop } from '@fortawesome/free-solid-svg-icons'
 import SkillsEntry from './SkillsEntry'
+import EntryForm from '../EntryForm'
 
 export default class Skills extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      skillsList: [],
+    }
+
+    this.addSkill = this.addSkill.bind(this)
+  }
+
+  addSkill(skill) {
+    this.setState((prevState, _) => {
+      const newSkillsList = [...prevState.skillsList, skill]
+      return { skillsList: newSkillsList }
+    })
+  }
+
+  removeSkill(index) {
+    this.setState((prevState, _) => {
+      const newSkillsList = [...prevState.skillsList]
+      newSkillsList.splice(index, 1)
+      return { skillsList: newSkillsList }
+    })
+  }
+
   render() {
+    const { skillsList } = this.state
+    const skillsEntryFields = [{ label: 'Skill', type: 'text', name: 'skill' }]
+
     return (
       <section id="skills">
         <h2>
@@ -13,12 +42,11 @@ export default class Skills extends Component {
         </h2>
 
         <div className="skills-entries">
-          <SkillsEntry skill="Graphic design" />
-          <SkillsEntry skill="Web design" />
-          <SkillsEntry skill="Video editing" />
-          <SkillsEntry skill="Branding" />
-          <SkillsEntry skill="Photography" />
-          <SkillsEntry skill="Marketing" />
+          <EntryForm fields={skillsEntryFields} onSubmit={this.addSkill} />
+
+          {skillsList.map((entry, i) => (
+            <SkillsEntry key={i} skill={entry.skill} removeSkill={() => this.removeSkill(i)} />
+          ))}
         </div>
       </section>
     )
--- src/components/skills/SkillsEntry.js
+++ src/components/skills/SkillsEntry.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export default class SkillsEntry extends Component {
   render() {
-    const { skill } = this.props
+    const { skill, removeSkill } = this.props
 
     return (
       <div className="skills-entry">
         <p>{skill}</p>
+
+        <button style={{ backgroundColor: 'inherit', color: 'grey', marginTop: 'unset' }}>
+          <FontAwesomeIcon icon={faXmark} onClick={removeSkill} />
+        </button>
       </div>
     )
   }
